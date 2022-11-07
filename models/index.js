40a2180489a601fd7e774a7808283b6ab7d47ee1
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-11-06 22:25:29 -0800

    random bug

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'Category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Product_Tag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tag_Product'
+});
 
 module.exports = {
   Product,
